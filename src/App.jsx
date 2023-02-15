aa4a50a24ba89389b4b088b365b8b4f0370dd4b3
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-15 16:19:56 -0300

    :sparkles: feat: implementing Calls context

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 
 import { Routes, Route } from 'react-router-dom';
 
+import { CallsProvider } from './contexts/CallsContext';
+import { AuthProvider } from './contexts/AuthContext';
+
 import Dashboard from './pages/Dashboard/Dashboard';
 import Login from './pages/Login/Login';
 import NotFound from './pages/NotFound/NotFound';
-import { AuthProvider } from './contexts/AuthContext';
 import RequireAuth from './components/Auth/RequireAuth';
 
 
 function App() {
   return (
     <AuthProvider>
-    <Routes>
-        <Route path="/" element={
-          <RequireAuth>
-            <Dashboard />
-          </RequireAuth>
-        } />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-    </Routes>
+      <CallsProvider>
+        <Routes>
+            <Route path="/" element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            } />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+      </CallsProvider>
     </AuthProvider>
   );
 }
