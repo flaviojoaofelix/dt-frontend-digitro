485b3c658784dd2ac8e13ca8eb2d57b7e58fdc59
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-15 01:24:49 -0300

    :sparkles: feat: implementing login route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
