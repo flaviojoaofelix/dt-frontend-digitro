a5f6e235ddd8b794904a53dafba292e1030c137a
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-15 21:41:06 -0300

    :lipstick: feat: dashboard initial bootstrap layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -14,17 +14,16 @@ const socket = io('http://dev.digitro.com', {
   path: '/callcontrol',
   forceNew: true,
   reconnectionAttempts: 3,
-  timeout: 2000
+  timeout: 2000,
 });
 
 function Dashboard() {
   const [apiError, setApiError] = useState(false);
 
   const { addCall, cleanCalls, removeCall } = useContext(CallsContext);
-  const auth = useContext(AuthContext)
-  const navigate = useNavigate()
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  
   const connect = () => {
     socket.connect();
     socket.emit('USER_CONNECT', {
@@ -45,14 +44,14 @@ function Dashboard() {
   const endCall = (callId) => {
     socket.emit('END_CALL', {
       callId,
-    })
+    });
   };
 
   const handleError = ({ type, message }) => {
-    setApiError({ type, message })
+    setApiError({ type, message });
 
-    setTimeout(() => setApiError(false), 8000)
-  }
+    setTimeout(() => setApiError(false), 8000);
+  };
 
   useEffect(() => {
     if (auth.user) {
@@ -69,63 +68,75 @@ function Dashboard() {
 
     socket.on('USER_DISCONNECT', (data) => {
       console.log(data);
-    })
+    });
 
     socket.on('USER_DISCONNECTED', (data) => {
-      console.log(data)
-    })
+      console.log(data);
+    });
 
     socket.on('USER_DISCONNECTION_ERROR', ({ error }) => {
       handleError({ type: 'USER_DISCONNECTION_ERROR', message: error });
-    })
+    });
 
     socket.on('NEW_CALL', (data) => {
-      console.log(data)
-      addCall(data)
-    })
+      console.log(data);
+      addCall(data);
+    });
 
     socket.on('NEW_CALL_ANSWERED', (data) => {
-      console.log(data)
-    })
+      console.log(data);
+    });
 
     socket.on('NEW_CALL_ERROR', ({ error }) => {
       handleError({ type: 'NEW_CALL_ERROR', message: error });
-    })
+    });
 
     socket.on('END_CALL', (data) => {
-      console.log(data)
-    })
+      console.log(data);
+    });
 
     socket.on('CALL_ENDED', ({ callId }) => {
       removeCall(callId);
-    })
+    });
 
     socket.on('END_CALL_ERROR', ({ error }) => {
       handleError({ type: 'END_CALL_ERROR', message: error });
-    })
+    });
 
     return () => {
       socket.off('USER_CONNECTED');
       socket.off('USER_DISCONNECT');
       socket.off('USER_DISCONNECTED');
-      socket.off('USER_DISCONNECTION_ERROR')
-      socket.off('NEW_CALL')
-      socket.off('NEW_CALL_ANSWERED')
-      socket.off('NEW_CALL_ERROR')
-      socket.off('END_CALL')
-      socket.off('CALL_ENDED')
-      socket.off('END_CALL_ERROR')
-    }
-  }, [])
+      socket.off('USER_DISCONNECTION_ERROR');
+      socket.off('NEW_CALL');
+      socket.off('NEW_CALL_ANSWERED');
+      socket.off('NEW_CALL_ERROR');
+      socket.off('END_CALL');
+      socket.off('CALL_ENDED');
+      socket.off('END_CALL_ERROR');
+    };
+  }, []);
 
   return (
-    <div className="App">
-      <Header logout={disconnect} />
-      { apiError && (<span>{apiError.type}: {apiError.message}</span>) }
-      <CallsList />
-      <CallDetails endCall={endCall} />
-    </div>
+    <>
+      <Header signOut={disconnect} />
+      <main className="container text-center p-3">
+        <div className="row bg-white rounded-1">
+          <section className="col-sm-4 border-end">
+            <CallsList />
+          </section>
+          <section className="col-sm-8">
+            {apiError && (
+              <span>
+                {apiError.type}: {apiError.message}
+              </span>
+            )}
+            <CallDetails endCall={endCall} />
+          </section>
+        </div>
+      </main>
+    </>
   );
-};
+}
 
 export default Dashboard;
