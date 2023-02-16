3960c47339f212e2729152611503912ebc78e367
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-16 02:39:37 -0300

    :lipstick: feat: calls details component bootstrap layout

--- src/components/Calls/CallDetails.jsx
+++ src/components/Calls/CallDetails.jsx
@@ -9,21 +9,61 @@ function CallDetails({ endCall }) {
   const handleEndCall = () => {
     endCall(selectedCall.callId);
     deselectCall();
-  }
+  };
 
-  return(
+  return (
     <div>
-      <h2>Detalhes da Chamada:</h2>
-      { selectedCall ? (
+      <h2 className="display-6 text-center">Detalhes da Chamada</h2>
+      <hr />
+      {selectedCall ? (
         <>
-          <span>Call ID: {selectedCall.callId}</span>
-          <span>Mídia: {selectedCall.media}</span>
-          <span>Data inicial: {selectedCall.startDate}</span>
-          <span>Serviço: {selectedCall.service}</span>
-          <span>Origem: {selectedCall.caller}</span>
-          <input type="button" id="endCall" name="endCall" value="Finalizar" onClick={handleEndCall} />
+          <div className="card">
+            <div className="card-header">
+              <div className="row align-items-center">
+                <div className="col-10">
+                  <h3 className="fs-4 mb-0 lead">
+                    <i className="bi bi-telephone"> </i>
+                    Call ID: {selectedCall.callId}
+                  </h3>
+                </div>
+                <div className="col text-end">
+                  <button type="button" className="btn-close" aria-label="Close" onClick={deselectCall} />
+                </div>
+              </div>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item text-align-start">
+                <i className="bi bi-mic"> </i>
+                <strong>Mídia:</strong> {selectedCall.media}
+              </li>
+              <li className="list-group-item">
+                <i className="bi bi-calendar-event"> </i>
+                <strong>Data inicial:</strong> {selectedCall.startDate}
+              </li>
+              <li className="list-group-item">
+                <i className="bi bi-building"> </i>
+                <strong>Serviço:</strong> {selectedCall.service}
+              </li>
+              <li className="list-group-item">
+                <i className="bi bi-person"> </i>
+                <strong>Origem:</strong> {selectedCall.caller}
+              </li>
+            </ul>
+            <div className="card-footer text-end">
+              <input
+                type="button"
+                id="endCall"
+                name="endCall"
+                className="btn btn-primary"
+                value="Finalizar"
+                onClick={handleEndCall}
+              />
+            </div>
+          </div>
         </>
-      ) : (<h3>Nenhuma chamada selecionada</h3>)}
+      ) : (
+        <h3 className="lead text-center">Nenhuma chamada selecionada</h3>
+      )}
     </div>
   );
 }
