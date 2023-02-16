8bc50f1a2f18637ae5fee9fd56ca6d8ce3f18e69
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-16 12:17:15 -0300

    :sparkles: feat: implementing alert component at call list

--- src/components/Calls/CallsList.jsx
+++ src/components/Calls/CallsList.jsx
@@ -1,16 +1,19 @@
 import { useContext } from 'react';
+import PropTypes from 'prop-types';
 
 import { CallsContext } from '../../contexts/CallsContext';
 
 import CallListCard from './CallListCard';
+import Alerts from '../Layout/Alerts';
 
-function CallsList() {
+function CallsList({ alertMessage }) {
   const { calls, selectCall, selectedCall } = useContext(CallsContext);
 
   return (
     <div>
       <h2 className="display-6 text-center">Atendimentos</h2>
       <hr />
+      {alertMessage && <Alerts alertMessage={alertMessage} />}
       {calls.length ? (
         calls.map((call) => (
           <CallListCard key={call.callId} call={call} selectCall={selectCall} selectedCall={selectedCall} />
@@ -26,5 +29,20 @@ function CallsList() {
     </div>
   );
 }
+
+CallsList.propTypes = {
+  alertMessage: PropTypes.oneOfType([
+    PropTypes.shape({
+      type: PropTypes.string,
+      title: PropTypes.string,
+      message: PropTypes.string,
+    }),
+    PropTypes.bool,
+  ]),
+};
+
+CallsList.defaultProps = {
+  apiError: false,
+};
 
 export default CallsList;
