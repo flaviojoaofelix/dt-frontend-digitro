f67e8e887f39f2cbb0c59492535fdc0854cbf768
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-17 09:53:24 -0300

    :wrench chore: changes to deploy on github pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -20,20 +20,24 @@ function Login() {
     auth.signIn(formData, () => {
       navigate(from, { replace: true });
     });
-  }
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData((prevState) => ({...prevState, [name]: value }))
-  }
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-  return(
+  return (
     <div className="container">
       <div className="row">
         <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
           <div className="card border-0 shadow rounded-3 my-5">
             <div className="card-body p-4 p-sm-5">
-              <img src="/logo-digitro.png" className="card-img-top mx-auto d-block responsive-image" alt="Logotipo Dígitro Tecnologia" />
+              <img
+                src="https://flaviojoaofelix.dev/dt-frontend-digitro/assets/images/logo-digitro.png"
+                className="card-img-top mx-auto d-block responsive-image"
+                alt="Logotipo Dígitro Tecnologia"
+              />
               <h4 className="card-title text-center mb-3 fw-light fs-5">Call Control</h4>
               <hr className="my-4"></hr>
               <h5 className="card-title text-center mb-4 fw-light fs-2">Sign In</h5>
@@ -64,7 +68,9 @@ function Login() {
                   <label htmlFor="maxCalls">Limite Máximo de Chamadas</label>
                 </div>
                 <div className="d-grid">
-                  <button type="submit" className="btn btn-primary btn-login text-uppercase fw-bold">Conectar</button>
+                  <button type="submit" className="btn btn-primary btn-login text-uppercase fw-bold">
+                    Conectar
+                  </button>
                 </div>
               </form>
             </div>
