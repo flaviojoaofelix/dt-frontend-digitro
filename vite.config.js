42980bfef5047d048825952ed4e6e737a3b083bd
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-17 10:48:19 -0300

    :sparkles: feat: files for run the application with Docker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dt-frontend-digitro/',
+  base: '/',
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
 });
