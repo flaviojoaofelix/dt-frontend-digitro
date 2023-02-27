628115d6237837c40713e0195950854055d57ff9
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-27 17:03:32 -0300

    :sparkles: feat: creating layout footer component

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="fixed-bottom align-items-center bg-dark text-light text-center pt-1">
+      <p className="m-0 p-0">
+        <i className="bi bi-c-circle"> </i>Dígitro e seu logotipo é uma marca registrada e pertence exclusivamente a{' '}
+        <a href="https://www.digitro.com/" target="_blank" className="link-primary" rel="noreferrer">
+          Dígitro Tecnologia
+        </a>{' '}
+        sendo utilizada aqui apenas como referência.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
