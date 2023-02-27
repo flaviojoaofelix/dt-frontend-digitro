06303ed529f13539e50c52afeb1824051ca88510
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-27 18:21:14 -0300

    :lipstick: feat: styling page not found (404)

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom';
+
 function NotFound() {
-  return(
-    <div>
-      <h1>404 - Not Found</h1>
-    </div>
+  return (
+    <main className="d-flex align-items-center justify-content-center vh-100">
+      <section className="text-center bg-light rounded p-3 shadow">
+        <h1 className="display-1 fw-bold">404</h1>
+        <hr />
+        <p className="fs-3">Página não encontrada!</p>
+        <p className="lead">O endereço da página que você tentou acessar parece não existir...</p>
+        <Link to="/" className="btn btn-primary">
+          Início
+        </Link>
+      </section>
+    </main>
   );
 }
 
